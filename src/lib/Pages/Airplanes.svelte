717e7f1d124c9da75c91386fdd71e005454a9ff4
seth0xd <naseem-chettah@proton.me>
2024-08-18 14:40:50 +0100

    first page done, added a refresh button to the table

--- src/lib/Pages/Airplanes.svelte
+++ src/lib/Pages/Airplanes.svelte
@@ -1,23 +1,69 @@
 <script>
   import { invoke } from "@tauri-apps/api/tauri";
+  import { onMount } from "svelte";
 
-  import Table from "../Table/Table.svelte";
-  let cols = ["model", "man", "capacity"];
-
-  let airplanes;
-  let showTable = false;
+  let cols = ["Model", "Manufacturer", "Capacity"];
+  let airplanes = [];
+  let loading = false;
+  let error = null;
 
   async function getAirplanes() {
-    airplanes = JSON.parse(await invoke("fetch_airplanes_from_db", {}));
-    showTable = true;
+    loading = true;
+    error = null;
+    try {
+      airplanes = JSON.parse(await invoke("fetch_airplanes_from_db", {}));
+    } catch (err) {
+      error = "Error fetching airplanes: " + err.message;
+    } finally {
+      loading = false;
+    }
   }
+
+  onMount(() => {
+    getAirplanes();
+  });
 </script>
 
 <div>
-  <button on:click={getAirplanes}>click</button>
-  <p>{airplanes}</p>
-
-  {#if showTable}
-    <Table {cols} rows={airplanes} />
+  <div class="flex justify-center">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <i
+      on:click={getAirplanes}
+      class={loading
+        ? "fa-solid fa-arrows-rotate fa-spin"
+        : "fa-solid fa-arrows-rotate"}
+    ></i>
+  </div>
+  {#if error}
+    <p class="text-red-600">{error}</p>
   {/if}
+  <div class="flex justify-center">
+    <table class="table p-4 bg-white rounded-lg shadow">
+      <thead>
+        <tr>
+          {#each cols as col}
+            <th
+              class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900"
+            >
+              {col}
+            </th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each airplanes as airplane}
+          <tr class="text-gray-700">
+            <td class="border-b-2 p-4 dark:border-dark-5">{airplane.model}</td>
+            <td class="border-b-2 p-4 dark:border-dark-5"
+              >{airplane.manufacturer}</td
+            >
+            <td class="border-b-2 p-4 dark:border-dark-5"
+              >{airplane.capacity}</td
+            >
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
