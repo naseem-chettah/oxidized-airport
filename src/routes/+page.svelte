5f652f88efbc1e0049a8ab3d9c9dfa8642dfa65f
seth0xd <naseem-chettah@proton.me>
2024-09-09 15:00:04 +0100

    starting over the front end

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,8 @@
   import { invoke } from "@tauri-apps/api/tauri";
   import { onMount } from "svelte";
 
-  import Header from "../lib/Header.svelte";
-  import Airplanes from "../lib/Pages/Airplanes.svelte";
-  import Airports from "../lib/Pages/Airports.svelte";
-  import Passengers from "../lib/Pages/Passengers.svelte";
-  import Flights from "../lib/Pages/Flights.svelte";
-
   let dbConnectionError = null;
   let loading = false;
-
-  let tabs = ["Airplanes", "Airports", "Passengers", "Flights"];
-  let activeTab = "Airplanes";
-
-  const tabChange = (e) => {
-    activeTab = e.detail;
-  };
 
   async function checkConnection() {
     dbConnectionError = null;
@@ -43,21 +30,13 @@
       <h1 class="text-4xl font-bold">ERROR 503</h1>
       <p class="mt-4 text-center">
         We faced an error connecting to the database, please check your
-        connection and <button class="underline" on:click={checkConnection}>try again</button> later.
+        connection and <button class="underline" on:click={checkConnection}
+          >try again</button
+        > later.
       </p>
     </div>
   {:else if !dbConnectionError}
-    <Header {tabs} {activeTab} on:tabChange={tabChange} />
-    <br />
-    {#if activeTab === "Airplanes"}
-      <Airplanes />
-    {:else if activeTab === "Airports"}
-      <Airports />
-    {:else if activeTab === "Passengers"}
-      <Passengers />
-    {:else if activeTab === "Flights"}
-      <Flights />
-    {/if}
+    hello
   {/if}
 {:else}
   <div
